b68113a6c4be46072c099e9cfcb0a510548bc6a4
Lorenzo Pollastrini <lorenzo.pollastrini@outlook.com>
2022-06-03 10:48:41 +0200

    Modificato lo stile delle pagine contenenti form

--- src/main/java/it/uniroma3/siw/siwcatering/controller/MainController.java
+++ src/main/java/it/uniroma3/siw/siwcatering/controller/MainController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.siw.siwcatering.model.Buffet;
 import it.uniroma3.siw.siwcatering.model.Chef;
-import it.uniroma3.siw.siwcatering.model.Ingrediente;
 import it.uniroma3.siw.siwcatering.model.Piatto;
 import it.uniroma3.siw.siwcatering.service.BuffetService;
 import it.uniroma3.siw.siwcatering.service.ChefService;
@@ -30,7 +28,7 @@ public class MainController {
 
 	@GetMapping("/")
 	public String home() {
-		return "home";
+		return "redirect:/chef";
 	}
 	
 	@GetMapping("/admin")
@@ -45,48 +43,6 @@ public class MainController {
 		model.addAttribute("piatti", piatti);
 		
 		return "admin-home";
-		
-	}
-	
-	@GetMapping("/admin/chef/{chefId}")
-	public String getAdminChefView(@PathVariable("chefId") Long chefId,
-			Model model) {
-		
-		Chef chef = chefService.findById(chefId);
-		Collection<Buffet> buffets = chef.getBuffet();
-		
-		model.addAttribute("chef", chef);
-		model.addAttribute("buffets", buffets);
-		
-		return "admin-chef";
-		
-	}
-	
-	@GetMapping("/admin/buffet/{buffetId}")
-	public String getAdminBuffetView(@PathVariable("buffetId") Long buffetId,
-			Model model) {
-		
-		Buffet buffet = buffetService.findById(buffetId);
-		Collection<Piatto> piatti = buffet.getPiatti();
-		
-		model.addAttribute("buffet", buffet);
-		model.addAttribute("piatti", piatti);
-		
-		return "admin-buffet";
-		
-	}
-	
-	@GetMapping("/admin/piatto/{piattoId}")
-	public String getAdminPiattoView(@PathVariable("piattoId") Long piattoId,
-			Model model) {
-		
-		Piatto piatto = piattoService.findById(piattoId);
-		Collection<Ingrediente> ingredienti = piatto.getIngredienti();
-		
-		model.addAttribute("piatto", piatto);
-		model.addAttribute("ingredienti", ingredienti);
-		
-		return "admin-piatto";
 		
 	}
 	
